3963f2b090c4ed0fb8f2d1919c54e6b71a11a81e
Syed Ziauddin <syed.nlp00@gmail.com>
2022-02-16 22:01:20 +0530

    Update download.js

--- js/download.js
+++ js/download.js
@@ -8,15 +8,6 @@ function generatePDF(){
             image: {
                 type: 'jpeg',
                 quality: 0.98
-            },
-            html2canvas: {
-                scale: 3,
-                letterRendering: true,
-            },
-            jsPDF: {
-                unit: "in",
-                formate: "a3",
-                orientation: 'portrait'
             }
         })
     .from(page)
