3849e0ccf6a73ba996c0a409a24d27ba0c4fc6c9
Syed Ziauddin <syed.nlp00@gmail.com>
2023-10-01 03:31:44 +0530

    'fix'

--- service-worker.js
+++ service-worker.js
@@ -1,53 +1,53 @@
-// importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js');
 
-// workbox.routing.registerRoute(
-//     ({request}) => request.destination == 'image',
-//     new workbox.strategies.NetworkFirst()
-// );
+workbox.routing.registerRoute(
+    ({request}) => request.destination == 'image',
+    new workbox.strategies.NetworkFirst()
+);
 
-const CACHE_NAME = "version-1";
-const urlsToCache = [ 'index.htm', 'logo.png', 'js/script.js','/img/logo/logo.png', 'css/style.css' ];
+// const CACHE_NAME = "version-1";
+// const urlsToCache = [ 'index.htm', 'logo.png', 'js/script.js','/img/logo/logo.png', 'css/style.css' ];
 
-const self = this;
+// const self = this;
 
-// Install SW
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => {
-                console.log('Opened cache');
+// // Install SW
+// self.addEventListener('install', (event) => {
+//     event.waitUntil(
+//         caches.open(CACHE_NAME)
+//             .then((cache) => {
+//                 console.log('Opened cache');
 
-                return cache.addAll(urlsToCache);
-            })
-    )
-});
+//                 return cache.addAll(urlsToCache);
+//             })
+//     )
+// });
 
-// Listen for requests
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(() => {
-                return fetch(event.request) 
-                    .catch(() => caches.match('index.htm'))
-            })
-    )
-});
+// // Listen for requests
+// self.addEventListener('fetch', (event) => {
+//     event.respondWith(
+//         caches.match(event.request)
+//             .then(() => {
+//                 return fetch(event.request) 
+//                     .catch(() => caches.match('index.htm'))
+//             })
+//     )
+// });
 
-// Activate the SW
-self.addEventListener('activate', (event) => {
-    const cacheWhitelist = [];
-    cacheWhitelist.push(CACHE_NAME);
+// // Activate the SW
+// self.addEventListener('activate', (event) => {
+//     const cacheWhitelist = [];
+//     cacheWhitelist.push(CACHE_NAME);
 
-    event.waitUntil(
-        caches.keys().then((cacheNames) => Promise.all(
-            cacheNames.map((cacheName) => {
-                if(!cacheWhitelist.includes(cacheName)) {
-                    return caches.delete(cacheName);
-                }
-            })
-        ))
+//     event.waitUntil(
+//         caches.keys().then((cacheNames) => Promise.all(
+//             cacheNames.map((cacheName) => {
+//                 if(!cacheWhitelist.includes(cacheName)) {
+//                     return caches.delete(cacheName);
+//                 }
+//             })
+//         ))
             
-    )
-});
+//     )
+// });
 
 
