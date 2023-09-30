7ce7b8b7a84d5442067a2402385eecdaa44b25b8
Syed Ziauddin <syed.nlp00@gmail.com>
2023-10-01 03:17:22 +0530

    'offline feature'

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,53 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js');
+// importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js');
 
-workbox.routing.registerRoute(
-    ({request}) => request.destination == 'image',
-    new workbox.strategies.NetworkFirst()
-);
+// workbox.routing.registerRoute(
+//     ({request}) => request.destination == 'image',
+//     new workbox.strategies.NetworkFirst()
+// );
+
+const CACHE_NAME = "version-1";
+const urlsToCache = [ 'index.html', 'logo.png', 'js/script.js','/img/logo/logo.png', 'css/style.css' ];
+
+const self = this;
+
+// Install SW
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Opened cache');
+
+                return cache.addAll(urlsToCache);
+            })
+    )
+});
+
+// Listen for requests
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(() => {
+                return fetch(event.request) 
+                    .catch(() => caches.match('index.html'))
+            })
+    )
+});
+
+// Activate the SW
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [];
+    cacheWhitelist.push(CACHE_NAME);
+
+    event.waitUntil(
+        caches.keys().then((cacheNames) => Promise.all(
+            cacheNames.map((cacheName) => {
+                if(!cacheWhitelist.includes(cacheName)) {
+                    return caches.delete(cacheName);
+                }
+            })
+        ))
+            
+    )
+});
+
 
